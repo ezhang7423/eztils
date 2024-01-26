6b508bd3ae55d6e6a269a5e2a0eb7c9240567e0b
ezhang7423 <ezhang7423@student.palomar.edu>
2024-01-26 11:52:23 -0500

    add persistence - huggingface

---
+++ eztils/persistence/hf.py
@@ -0,0 +1,26 @@
+"""
+Requires setting HF_TOKEN env variable
+"""
+
+import os
+from pathlib import Path
+from huggingface_hub import create_repo, upload_folder, login
+
+
+def upload_folder(local_folder: str, remote: str, username="ezipe"):
+    """Upload a folder to a huggingface repo
+
+    :param local_folder: This should be the data root. The folder will be uploaded to the repo root.
+    Files already existing in the remote will overwritten.
+    iles existing in the remote but not in the local will NOT be deleted.
+    Files existing in the local but not in the remote will be uploaded.
+    """
+    login(token=os.environ["HF_TOKEN"])
+    lf = Path(local_folder).resolve()
+    try:
+        create_repo(repo_id=remote)
+    except:
+        pass
+
+    print(f"Uploading folder {lf} to huggingface {remote}...")
+    upload_folder(folder_path=lf, repo_id=f"{username}/{remote}")
